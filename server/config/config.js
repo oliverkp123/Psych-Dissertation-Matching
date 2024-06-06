4277e6dcbb83f93beedc8b0756507a9906cb1844
oliverkp123 <kochpaizoliver@gmail.com>
2024-06-06 01:19:10 -0400

    Fix

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,19 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   }
 };
