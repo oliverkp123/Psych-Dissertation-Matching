db198a364b61ad494e57d24458b110cc14d433b4
oliverkp123 <kochpaizoliver@gmail.com>
2024-06-06 01:12:09 -0400

    removed wrong database

--- server/config/config.js
+++ server/config/config.js
@@ -7,19 +7,5 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD || null,
-    database: 'database_test',
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD || null,
-    database: 'database_production',
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
   }
 };
