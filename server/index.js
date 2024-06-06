e9592a8908b6bf94d8fb29b12c0939ffe725d621
oliverkp123 <kochpaizoliver@gmail.com>
2024-06-06 02:02:52 -0400

    Changed cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://psych-dissertation-matching.vercel.app/",
     credentials: true,
   })
 );
